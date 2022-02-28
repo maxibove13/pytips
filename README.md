e9042918cfd7b7ef009a067a976746e72b1a30c9
maxibove13 <maxibove13@gmail.com>
2022-02-28 16:20:40 -0300

    reloading modules

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 Some Python tips and tricks for writing better code.
 Work in progress.
 
-
 Tips in `pytips.ipynb`
---
+++ testmod.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""This an example of a python module"""
+
+__author__ = "Maximiliano Bove"
+__email__ = "maxibove13@gmail.com"
+__status__ = "Development"
+__date__ = "02/22"
+
+C = 6
